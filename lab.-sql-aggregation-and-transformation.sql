21c180b9aa1cde8519a9822ff3e8304a1b97cb63
estebanba <34803146+estebanba@users.noreply.github.com>
2025-02-05 07:09:57 +0100

    Solved Lab

--- lab.-sql-aggregation-and-transformation.sql
+++ lab.-sql-aggregation-and-transformation.sql
@@ -5,13 +5,14 @@
 SELECT MAX(length) AS max_duration, MIN(length) AS min_duration FROM film;
 -- 	- 1.2. Express the **average movie duration in hours and minutes**. Don't use decimals.
 --       - *Hint: Look for floor and round functions.*
-SELECT FLOOR(AVG(length)) AS average_duration FROM film;
+-- SELECT FLOOR(AVG(length)) AS average_duration FROM film;
+SELECT FLOOR(AVG(length) / 60) AS hours, ROUND(AVG(length) % 60) AS minutes FROM film;
 -- 2. You need to gain insights related to rental dates:
 -- 	- 2.1 Calculate the **number of days that the company has been operating**.
 --       - *Hint: To do this, use the `rental` table, and the `DATEDIFF()` function to subtract the earliest date in the `rental_date` column from the latest date.*
-SELECT DATEDIFF( MIN(rental.rental_date), MAX(rental.rental_date)) AS operating_days FROM rental;
+SELECT DATEDIFF(MAX(rental.rental_date), MIN(rental.rental_date)) AS operating_days FROM rental;
 -- 	- 2.2 Retrieve rental information and add two additional columns to show the **month and weekday of the rental**. Return 20 rows of results.
-SELECT *, MONTH(rental_date) AS month, WEEKDAY(rental_date) AS weekday FROM rental;
+SELECT *, MONTH(rental_date) AS month, WEEKDAY(rental_date) AS weekday FROM rental LIMIT 20;
 -- 	- 2.3 *Bonus: Retrieve rental information and add an additional column called `DAY_TYPE` with values **'weekend' or 'workday'**, depending on the day of the week.*
 --       - *Hint: use a conditional expression.*
 SELECT *, WEEKDAY(rental_date) AS weekday,
@@ -28,7 +29,8 @@ CASE
 	WHEN ISNULL(rental_duration) THEN "Not Available"
     ELSE rental_duration
 END AS rental_duration
-FROM film;
+FROM film
+ORDER BY title ASC;
 
 -- 4. *Bonus: The marketing team for the movie rental company now needs to create a personalized email campaign for customers. To achieve this, you need to retrieve the **concatenated first and last names of customers**, along with the **first 3 characters of their email** address, so that you can address them by their first name and use their email address to send personalized recommendations. The results should be ordered by last name in ascending order to make it easier to use the data.*
 
@@ -46,6 +48,7 @@ SELECT rating, COUNT(*) AS total_films FROM film GROUP BY rating ORDER BY total_
 --  - 2.1 The **mean film duration for each rating**, and sort the results in descending order of the mean duration. Round off the average lengths to two decimal places. This will help identify popular movie lengths for each category.
 SELECT rating, FLOOR(AVG(length)) AS average_duration FROM film GROUP BY rating ORDER BY average_duration DESC;
 -- 	- 2.2 Identify **which ratings have a mean duration of over two hours** in order to help select films for customers who prefer longer movies.
-SELECT rating, FLOOR(AVG(length)) AS average_duration FROM film WHERE length > 120 GROUP BY rating ORDER BY average_duration DESC;
+-- SELECT rating, ROUND(AVG(length),2) AS average_duration FROM film WHERE length > 120 GROUP BY rating ORDER BY average_duration DESC;
+SELECT rating, ROUND(AVG(length),2) AS avg_duration FROM film GROUP BY rating HAVING avg_duration > 120 ORDER BY avg_duration DESC;
 -- 3. *Bonus: determine which last names are not repeated in the table `actor`.*
-SELECT first_name, COUNT(*) count FROM actor GROUP BY first_name HAVING count = 1;
+SELECT last_name, COUNT(*) count FROM actor GROUP BY last_name HAVING count = 1;
